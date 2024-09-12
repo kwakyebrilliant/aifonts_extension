88ae6b0c2a56b94b9278ce90f779e58bdbf2b137
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-09-12 10:42:00 +0300

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Chrome Extension',
+      title: 'aiFonts',
       home: MyHomePage(),
     );
   }
@@ -19,7 +20,28 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Chrome Extension'),
+        title: RichText(
+          text: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: 'ai',
+                style: GoogleFonts.inter(
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xFF800080),
+                ),
+              ),
+              TextSpan(
+                text: 'fonts',
+                style: GoogleFonts.roboto(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.w800,
+                  color: const Color(0xFF000000),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: Center(
         child: ElevatedButton(
